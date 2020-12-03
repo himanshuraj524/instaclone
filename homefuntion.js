bac39cf4885abf492ddba7323b05ccba9553de81
Himanshu <himanshurajverma549@gmail.com>
2020-12-03 11:13:24 +0530

    video completed

--- homefuntion.js
+++ homefuntion.js
@@ -1,16 +1,21 @@
+/**buttons**/
 let home = document.getElementById('home');
 let video = document.getElementById('video');
 let post = document.getElementById('post');
 let likes = document.getElementById('likes');
 let user_profile = document.getElementById('user-profile');
+let search = document.getElementById('search');
 
+/*content blocks*/
 const navbar = document.querySelector('.navbar');
 const contentbar = document.querySelector('.contentbar');
 const videopage = document.querySelector('.videopage');
 const postpage = document.querySelector('.postpage');
 const likepage = document.querySelector('.likepage');
 const profilepage = document.querySelector('.profilepage');
+const searchpage = document.querySelector('.searchpage');
 
+/*Event listeners functions*/
 
 window.addEventListener('load', function(){
     navbar.style.display = 'block';
@@ -19,6 +24,18 @@ window.addEventListener('load', function(){
     postpage.style.display = 'none';
     likepage.style.display = 'none';
     profilepage.style.display = 'none';
+    searchpage.style.display = 'none'; 
+
+});
+
+search.addEventListener('click', function(){
+    navbar.style.display = 'none';
+    contentbar.style.display = 'none';
+    videopage.style.display = 'none';
+    postpage.style.display = 'none';
+    likepage.style.display = 'none';
+    profilepage.style.display = 'none';
+    searchpage.style.display = 'block'; 
 });
 
 home.addEventListener('click', function(){
@@ -28,6 +45,8 @@ home.addEventListener('click', function(){
     postpage.style.display = 'none';
     likepage.style.display = 'none';
     profilepage.style.display = 'none';
+    searchpage.style.display = 'none'; 
+
 });
 
 video.addEventListener('click', function(){
@@ -37,6 +56,8 @@ video.addEventListener('click', function(){
     postpage.style.display = 'none';
     likepage.style.display = 'none';
     profilepage.style.display = 'none';
+    searchpage.style.display = 'none'; 
+
 });
 
 post.addEventListener('click', function(){
@@ -46,6 +67,8 @@ post.addEventListener('click', function(){
     postpage.style.display = 'block';
     likepage.style.display = 'none';
     profilepage.style.display = 'none';
+    searchpage.style.display = 'none'; 
+
 });
 
 likes.addEventListener('click', function(){
@@ -55,6 +78,8 @@ likes.addEventListener('click', function(){
     postpage.style.display = 'none';
     likepage.style.display = 'block';
     profilepage.style.display = 'none';
+    searchpage.style.display = 'none'; 
+
 });
 
 user_profile.addEventListener('click', function(){
@@ -64,4 +89,6 @@ user_profile.addEventListener('click', function(){
     postpage.style.display = 'none';
     likepage.style.display = 'none';
     profilepage.style.display = 'block';
+    searchpage.style.display = 'none'; 
+
 });
